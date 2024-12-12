c04e3446bdc2e3dd77ca90909e92388aae52ac0e
Anders Rask <144682594+RaskStudio@users.noreply.github.com>
2024-12-12 15:14:40 +0100

    kalender error

--- src/app/(flows)/calendar/page.tsx
+++ src/app/(flows)/calendar/page.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Link from "next/link";
+import { toast } from "sonner"
 
 export default function CalenderPage() {
   return (
@@ -8,9 +10,10 @@ export default function CalenderPage() {
         <Link href={"/calendar/2"}><img className="w-20 h-20 fixed top-50 left-32" src="/cup2.png" alt="kalender" /></Link>
         <Link href={"/calendar/3"}><img className="w-20 h-20 fixed top-50 right-32" src="/cup3.png" alt="kalender" /></Link>
         <Link href={"/calendar/4"}><img className="w-20 h-20 fixed top-56 right-20" src="/cup4.png" alt="kalender" /></Link>
+        <div onClick={() => toast("Det er vidst ikke dagens dato du har valgt ;)")}>
         <img src="/julekalender.png" alt="kalender" />
-        </div>
-      <div className="fixed bottom-4 right-4 flex flex-row w-24 h-12 bg-bill-orange rounded-full items-center p-4">
+      </div>
+    </div><div className="fixed bottom-4 right-4 flex flex-row w-24 h-12 bg-bill-orange rounded-full items-center p-4">
         <img src="/share-icon.svg" alt="share" />
         <p className="text-white">Del</p>
       </div>
