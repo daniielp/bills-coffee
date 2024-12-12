5f0f8783f2b5e187634b594830b4955ca45f7cd2
Anders Rask <144682594+RaskStudio@users.noreply.github.com>
2024-12-12 14:43:07 +0100

    kalendersiden

--- src/app/(flows)/calendar/page.tsx
+++ src/app/(flows)/calendar/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link";
+
 export default function CalenderPage() {
   return (
     <main>
+      <div className="flex p-4">
+        <Link href={"/calendar/1"}><img className="w-20 h-20 fixed top-50 left-12" src="/cup1.png" alt="kalender" /></Link>
+        <Link href={"/calendar/2"}><img className="w-20 h-20 fixed top-50 left-32" src="/cup2.png" alt="kalender" /></Link>
+        <Link href={"/calendar/3"}><img className="w-20 h-20 fixed top-50 right-32" src="/cup3.png" alt="kalender" /></Link>
+        <Link href={"/calendar/4"}><img className="w-20 h-20 fixed top-56 right-20" src="/cup4.png" alt="kalender" /></Link>
+        <img src="/julekalender.png" alt="kalender" />
+        </div>
       <div className="fixed bottom-4 right-4 flex flex-row w-24 h-12 bg-bill-orange rounded-full items-center p-4">
         <img src="/share-icon.svg" alt="share" />
         <p className="text-white">Del</p>
