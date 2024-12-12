23c7ffda24e0c894b0a12796e7ae60da7ea83898
jakobkonnerup <jakob.b.k.k@hotmail.comm>
2024-12-12 13:41:02 +0100

    Del knap styling på calendar-side
    :)

--- src/app/(flows)/calendar/page.tsx
+++ src/app/(flows)/calendar/page.tsx
@@ -1,5 +1,13 @@
 export default function CalenderPage() {
   return (
-    <div>CalenderPage</div>
+    <main>
+      <img src="/kalender-guilante.svg" alt="kalender" />
+      <div className="fixed bottom-4 right-4 flex flex-row w-24 h-12 bg-bill-orange rounded-full items-center p-4">
+        <img src="/share-icon.svg" alt="share" />
+        <p className="text-white">Del</p>
+      </div>
+    </main>
+
+
   )
 }
