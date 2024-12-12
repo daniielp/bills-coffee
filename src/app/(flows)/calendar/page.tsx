1f80c38f65827bda4bbfaef44a5fb9b052313a62
jakobkonnerup <jakob.b.k.k@hotmail.comm>
2024-12-12 13:42:55 +0100

    Update page.tsx

--- src/app/(flows)/calendar/page.tsx
+++ src/app/(flows)/calendar/page.tsx
@@ -1,7 +1,6 @@
 export default function CalenderPage() {
   return (
     <main>
-      <img src="/kalender-guilante.svg" alt="kalender" />
       <div className="fixed bottom-4 right-4 flex flex-row w-24 h-12 bg-bill-orange rounded-full items-center p-4">
         <img src="/share-icon.svg" alt="share" />
         <p className="text-white">Del</p>
